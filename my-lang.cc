9ee44bab27f667b4ed9a989e7b782e4543c33b21
JoyenBenitto <joyen.benitto12@gmail.com>
2023-11-28 12:28:43 +0530

    lexer test and cleanup

--- my-lang.cc
+++ my-lang.cc
@@ -26,63 +26,64 @@ static double NumVal;             // Filled in if tok_number
 /// gettok - Return the next token from standard input.
 static int gettok() {
   static int LastChar = ' ';
-
-  while (isspace(LastChar)){
+  // Skip any whitespace.
+  while (isspace(LastChar))
     LastChar = getchar();
-  }
+
+  if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+  IdentifierStr = LastChar;
   
-    if(isalpha(LastChar)){
-      IdentifierStr = LastChar;
-      while (isalnum((LastChar = getchar()))){
-          IdentifierStr += LastChar;
-      } 
-      if(IdentifierStr == "def"){
-          return tok_def;
-      }
-      if(IdentifierStr == "extern"){
-          return tok_extern;
-      }
-      return tok_identifier;
-    }
+  while (isalnum((LastChar = getchar())))
+    IdentifierStr += LastChar;
 
-    if(isdigit(LastChar) || LastChar == '.'){
-      std::string NumStr;
-      do{
-          NumStr += LastChar;
-          LastChar = getchar();
-      } while (isdigit(LastChar) || LastChar ==  '.');
-      
-      NumVal = strtod(NumStr.c_str(), 0);
-      return tok_number;
-    }
-
-    if (LastChar == '#') {
-      // Comment until end of line.
-      do
-        LastChar = getchar();
-      while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+  if (IdentifierStr == "def")
+    return tok_def;
+  
+  if (IdentifierStr == "extern")
+    return tok_extern;
     
-      if (LastChar != EOF)
-        return gettok();
-    }
+    return tok_identifier;
+  }
 
-      // Check for end of file.  Don't eat the EOF.
-    if (LastChar == EOF)
-      return tok_eof;   
-    // Otherwise, just return the character as its ascii value.
-    int ThisChar = LastChar;
+  if (isdigit(LastChar) || LastChar == '.') {   // Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+
+  if (LastChar == '#') {
+  // Comment until end of line.
+  do
     LastChar = getchar();
-    
-    return ThisChar;
+  while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+  if (LastChar != EOF)
+    return gettok();
+  }
+
+  // Check for end of file.  Don't eat the EOF.
+  if (LastChar == EOF)
+    return tok_eof;
+
+  // Otherwise, just return the character as its ascii value.
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
 }
 
 int main(){
-    int tok;
-    while (true){}{
-        tok = gettok();
-        {
-            std::cout <<"got token: "<<tok << std::endl;
-        }
+
+    std::cout <<"Hello from lexer"<< std::endl;
+    while (true){
+      int tok = gettok();
+      {
+          std::cout <<"got token: "<<tok << std::endl;
+      }
     }
     
     return 0;
